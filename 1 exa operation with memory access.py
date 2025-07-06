4ed86820173c8a18333df3709b6098d2eb3e443c
Khalid Shaikh <k@ruby.run>
2025-07-06 12:43:45 -0500

    u

--- 1 exa operation with memory access.py
+++ 1 exa operation with memory access.py
@@ -4,13 +4,8 @@
 # $env:PYOPENCL_CTX='0'
 
 # 1 exa operation
-# runs for less than 1 min and 40 seconds assuming 10 peta operations per second on GPU
-# no memory access, internal kernel variable gid
-
-# Processor	13th Gen Intel(R) Core(TM) i3-1315U, 1200 Mhz, 6 Core(s), 8 Logical Processor(s)
-# https://www.intel.com/content/www/us/en/products/sku/233459/intel-core-i31315u-processor-10m-cache-up-to-4-50-ghz-with-ipu/specifications.html
-# GPU 1.25GHz
-# EUs 64
+# runs for less than 5 seconds assuming 280 peta operations per second on GPU
+# memory access of input argument a[gid]
 
 import pyopencl as cl
 import numpy as np
