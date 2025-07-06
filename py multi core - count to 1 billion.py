5819ef4ad8a63b7ea08fb0a4eed077de04928385
Khalid Shaikh <k@ruby.run>
2025-07-05 22:45:09 -0500

    Added python count to 1 billion using multiple cores.

--- py multi core - count to 1 billion.py
+++ py multi core - count to 1 billion.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # python count to 1 billion 8 threads
 
-# 666 million operations per second
-
 import multiprocessing
 
 def count_range(start, end):
@@ -12,12 +10,8 @@ def count_range(start, end):
     return count
 
 if __name__ == '__main__':
-    total_count = int(5e9) 
-    # 12 seconds for 8 billion ( 1e9 * 8 ). 
-    # 55 seconds for 40 billion ( 5e9 * 8 ).
-
-    # 30 seconds for 1 trillion on GPU OpenCL 8-bit
-    # 50 seconds for 1 trillion on GPU OpenCL 32-bit
+    total_count = int(1e9) 
+ 
     num_processes = 8
     chunk_size = total_count // num_processes
 
